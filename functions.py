cecbb281c3e2f0d8da5ff95395399f7e45dc6cda
Yaniv_Mazor <mazya@post.bgu.ac.il>
2024-03-23 13:59:19 +0200

    Project

--- functions.py
+++ functions.py
@@ -14,26 +14,41 @@ import scipy.signal as ss
 #         if abs(teta[0] - teta[1]) > pram.delta:
 #             break
 #     return np.sort(teta)[::-1]
-
+def generate_qpsk_symbols(K,D):
+    random_symbols = np.random.randint(0, 4, (K,D))
+    qpsk_constellation = np.array([1+1j, -1+1j, -1-1j, 1-1j]) / np.sqrt(2)
+    qpsk_symbols = qpsk_constellation[random_symbols]
+    return qpsk_symbols.T
 def observ(SNR, K, A): #k=snapshots
     N = A.shape[0]
     D = A.shape[1]
-    real_s = np.random.normal(0, 1 / math.sqrt(2), (D, K))
-    im_s = np.random.normal(0, 1 / math.sqrt(2), (D, K))
-    s = real_s + 1j * im_s
+    # sample_rate = 1e6
+    # t = np.arange(K) / sample_rate  # time vector
+    # f_tone = np.array([0.02e3,0.08e3])
+    # s = np.exp(2j * np.pi * f_tone.reshape(2,1) * t.reshape(1,800))
+    # real_s = np.random.normal(1, 1 / math.sqrt(2), (D, K))
+    # im_s = np.random.normal(1, 1 / math.sqrt(2), (D, K))
+    # s = real_s + 1j * im_s
+    s = generate_qpsk_symbols(K,D)
     s_samp = s.reshape(D, K)
+
     real_n = np.random.normal(0, (10 ** (-SNR / 20)) / math.sqrt(2), (N, K))
     im_n = np.random.normal(0, (10 ** (-SNR / 20)) / math.sqrt(2), (N, K))
     n = real_n + 1j * im_n
     n_samp = n.reshape(N, K)
     x_a_samp = (A@s_samp) + n_samp
-    return x_a_samp.T.flatten()
+    return x_a_samp
 
 def GFT(S, x):
     eigenvalues, eigenvectors = np.linalg.eig(S)
     sorted_eigenvectors = eigenvectors[:, np.argsort(np.abs(eigenvalues))] #sorted in case of negative eigenvalues
-    x_tag = sorted_eigenvectors.T@x
-
+    x_tag = sorted_eigenvectors.T.conjugate()@x
     return x_tag
+
+def quantize(A, P, thresh_real=0, thresh_im=0):
+    mask = np.zeros(np.shape(A), dtype=complex)
+    mask[:P, :] = (1 / math.sqrt(2)) * (np.sign(A[:P, :].real - (thresh_real)) + (1j * (np.sign(A[:P, :].imag - ((thresh_im))))))
+    mask[P:, :] = A[P:, :]
+    return mask
 
 
--- main.py
+++ main.py
@@ -1,30 +1,77 @@
 import numpy as np
+from numpy import linalg as LA
 from matplotlib import pyplot as plt
+import scipy.signal as ss
 from classes import prameters_class, Matrix_class
-from functions import observ, GFT
+from functions import observ, GFT, quantize
 
-if __name__ == "__main__":
+if __name__ == "__main__": #TODO monte carlo
+    theta = [-15,32]
+    my_parameters = prameters_class(20,10,200,theta)
+    steering_original = Matrix_class(my_parameters).steering()
+    obs_a = observ(my_parameters.SNR, my_parameters.K, steering_original)
+    x_vec = quantize(obs_a, my_parameters.M)
 
-    D = 1 #TODO
-    N = 3 #sensors
-    teta = [20]
-    SNR = 0
-    k = 4 #samples
+    #Method 1:
+    # x_vec_s = x_vec.T.flatten()
+    # A_s_original = Matrix_class(my_parameters).Adjacency()
+    # x_tag_s_original = GFT(A_s_original,x_vec_s) #[np.abs(eigenvectors[:,i].T.conjugate()@np.tile(np.exp(-1j * np.pi * np.arange(4) * np.sin(np.radians(theta))),3)) for i in range(12)]
+    #
+    # my_parameters2 = prameters_class(my_parameters.M, my_parameters.SNR, my_parameters.K, my_parameters.D, [1])
+    # A_s = Matrix_class(my_parameters2).Adjacency()
+    # x_tag_s = GFT(A_s,x_vec_s)
+    #
+    # plt.figure(figsize=(10, 6))
+    # plt.stem(np.abs(x_tag_s_original) / np.max(np.abs(x_tag_s_original)), use_line_collection=True) #look only at \hat{x} abs
+    # plt.show()
+    # plt.figure(figsize=(10, 6))
+    # plt.stem(np.abs(x_tag_s)/np.max(np.abs(x_tag_s)), use_line_collection=True)
+    # plt.show()
 
-    monte = 100
-    delta = 5 #Minimal gap between two determenistic angles
-    Res = 1
+    # print(1/LA.norm(np.abs(np.delete(x_tag_s_original, np.argmax(np.abs(x_tag_s_original))))/np.max(np.abs(x_tag_s_original))))
+    # print(1 / LA.norm(np.abs(np.delete(x_tag_s, np.argmax(np.abs(x_tag_s)))) / np.max(np.abs(x_tag_s))))
 
-    my_parameters = prameters_class(N,SNR,k,D,teta,monte,delta,Res)
-    matrix = Matrix_class(my_parameters.N, my_parameters.k, teta)
-    A = matrix.Adjacency()
-    steering = matrix.steering()
-    x_vec = observ(my_parameters.SNR,my_parameters.k,steering)
-    x_tag = GFT(A,x_vec)
+    # Method 2:
+    # x_vec_s = np.average(x_vec,1)
+    # A_s_original = steering_original @ steering_original.T.conjugate() - (1/(my_parameters.M - 1))*np.eye(my_parameters.M)
+    # x_tag_s_original = GFT(A_s_original, x_vec_s)
+    #
+    # plt.figure(figsize=(10, 6))
+    # plt.stem(np.abs(x_tag_s_original) / np.max(np.abs(x_tag_s_original)), use_line_collection=True) #look only at \hat{x} abs
+    # plt.show()
+    # my_parameters2 = prameters_class(my_parameters.M, my_parameters.SNR, my_parameters.K,[70])
+    # steering = Matrix_class(my_parameters2).steering()
+    # A_s = steering @ steering.T.conjugate() - (1 / (my_parameters.M - 1)) * np.eye(my_parameters.M)
+    # x_tag_s = GFT(A_s, x_vec_s)
+    # plt.figure(figsize=(10, 6))
+    # plt.stem(np.abs(x_tag_s)/np.max(np.abs(x_tag_s)), use_line_collection=True)
+    # plt.show()
 
-    plt.figure(figsize=(10, 6))
-    plt.stem(np.abs(x_tag), use_line_collection=True)
-    plt.show()
+    def G_DOA(pram,teta_range):
+        x_vec_s = np.average(x_vec,1)#x_vec.T.flatten()
+        theta_range = np.arange(teta_range[0], teta_range[1], pram.Res)
+        spectrum = np.zeros(len(theta_range))
+        for idx, theta in enumerate(theta_range):
+            my_parameters2 = prameters_class(pram.M, pram.SNR, pram.K, [theta])
+            steering2 = Matrix_class(my_parameters2).steering()
+            A_s = steering2 @ steering2.T.conjugate() - (1 / (my_parameters.M - 1)) * np.eye(my_parameters.M)#Matrix_class(my_parameters2).Adjacency()
+            x_tag_s = GFT(A_s, x_vec_s)
+            spectrum[idx]= 1/LA.norm(np.abs(np.delete(x_tag_s, np.argmax(np.abs(x_tag_s))))/np.max(np.abs(x_tag_s)))
+        plt.plot(theta_range, spectrum,marker="*")
+        plt.show()
+
+        peaks, _ = ss.find_peaks(spectrum)
+        peaks = list(peaks)
+        peaks.sort(key=lambda x: spectrum[x])
+        pred = np.array(peaks[-pram.D:])
+        pred = np.sort(pred)[::-1]
+        return pred*pram.Res+teta_range[0]
+
+teta_range = [-60, 60]
+print(G_DOA(my_parameters,teta_range))
+
+
+
 
 
 
