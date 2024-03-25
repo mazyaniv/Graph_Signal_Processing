892b90540f59e26fca513bacd61631204107a1f3
mazyaniv <mazya@post.bgu.ac.il>
2024-03-25 09:32:53 +0200

    shshl

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from classes import prameters_class, Matrix_class
 from functions import observ, GFT, quantize, G_DOA
 
 if __name__ == "__main__": #TODO RMSE
-    my_parameters = prameters_class(20,0,200,[10,25,40],1,500)
+    my_parameters = prameters_class(20,0,200,[10,25,40],1,100)
     steering_original = Matrix_class(my_parameters).steering()
-    teta_range = [-60, 59]
+    teta_range = [-60, 60]
     print(G_DOA(my_parameters, teta_range, steering_original,my_parameters.M))
 
 # obs_a = observ(my_parameters.SNR, my_parameters.K, steering_original)
